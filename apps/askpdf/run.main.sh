cd952dc3f634aabf0af67edfe098eb4a63915183
niraj <niraj@mapmyindia.com>
2024-08-27 16:14:36 +0530

    update the run file with example instruction

--- apps/askpdf/run.main.sh
+++ apps/askpdf/run.main.sh
@@ -14,3 +14,15 @@ echo "Query for the file: $QUERY"
 
 # Run the Python script with the provided arguments
 python main.py --input_pdf "$PATH" --query "$QUERY"
+
+################################################################
+# Example usage:
+# create virtual environment using lsd-install-create.virtualenv or cond env
+# Install required packages pip install -r requirement.txt
+# Execute the commands
+# python main.py --input_pdf /codehub/aihub/external/practice/apps/askpdf/data/yolov10-2405.14458v1.pdf --query "What is the conclusion of this documents"
+
+
+# Output most Relevents chunks from docs 
+# We can use prompt templating to streamline the Q&A with docs 
+# Pass Docs and prompts TEMPLATE TO openAI API for answer !!!
